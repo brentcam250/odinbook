8a3eef915fc0ec699bc1b0f241125e173f76cd9a
Brent Cameron <bjcameron@gmail.com>
2021-02-16 12:54:45 -0700

    add friendrequest model

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,7 +1,15 @@
-class Friendship < ApplicationRecord
+class FriendRequest < ApplicationRecord
     belongs_to :requestor, class_name: 'User'
     belongs_to :requestee, class_name: 'User'
 
+    def accept
+        requestee.friends << requester
+        requester.friends << requestee 
+        self.destroy
+    end
 
+    def decline
+        self.destroy
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
+  #accepted friends
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
+  
+  #not accepted yet
+  has_many :sent_friend_requests, class_name: 'FriendRequest', foreign_key: :requester_id, dependent: :destroy
+  has_many :received_friend_requests, class_name: 'FriendRequest', foreign_key: :requestee_id, dependent: :destroy
   
 end
