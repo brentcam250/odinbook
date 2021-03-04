cf25f7ac3c0dad1f5d328d88ef288002b709ec2e
Brent Cameron <bjcameron@gmail.com>
2021-03-04 15:59:26 -0700

    filter posts so only a users friends posts show up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,13 @@ class PostsController < ApplicationController
     
     #this will act like timeline feature, sort newest posts for the currently logged in user
     def index
-        @posts = Post.all
+        #@posts = Post.all
+        if(current_user)
+            friends = current_user.friends.ids
+            @posts = Post.where(user_id: friends)
+        else
+            @posts = Post.first
+        end
     end
 
     def show
