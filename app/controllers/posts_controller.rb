20f1b6c322718243fe12e01af8756a13d1e0dcae
Brent Cameron <bjcameron@gmail.com>
2021-03-09 16:22:03 -0700

    creating post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
     
+    def new 
+        @post = Post.new
+    end
+
+    def create 
+        @post = Post.new(post_params)
+
+        respond_to do |format|
+          if @post.save
+            format.html { redirect_to @post, notice: 'post was successfully created.' }
+            format.json { render :show, status: :created, location: @post }
+          else
+            format.html { render :new }
+            format.json { render json: @post.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
     #this will act like timeline feature, sort newest posts for the currently logged in user
     def index
         #@posts = Post.all
@@ -22,6 +40,12 @@ class PostsController < ApplicationController
 
     def show
         @post = Post.find(params[:id])
+    end
+
+    private 
+
+    def post_params 
+        params.require(:post).permit(:user_id, :content)
     end
   end
   
