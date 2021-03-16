1ff13cd02ca485bee4a4f3bbc9187c5fcdc87ea3
Brent Cameron <bjcameron@gmail.com>
2021-03-16 10:44:48 -0600

    fix comment from from different locations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,15 @@ class CommentsController < ApplicationController
     
     def create 
         # @comment = Comment.new(comment_params)
-        @post = Post.find(params[:comment][:post])
-        @comment = @post.comments.build(content: params[:comment][:content], user_id: current_user.id)
+        #depending where comment is created from (show vs index) it may or may not come wrapped in comment param 
+        if params[:comment]
+            @post = Post.find(params[:comment][:post])
+            @comment = @post.comments.build(content: params[:comment][:content], user_id: current_user.id)
+
+        else 
+            @post = Post.find(params[:post])
+            @comment = @post.comments.build(content: params[:content], user_id: current_user.id)
+        end
         respond_to do |format|
 
             if (@comment.save)
