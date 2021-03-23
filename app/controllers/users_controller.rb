873791edc339e6d903796ceecb520e66751b2b44
Brent Cameron <bjcameron@gmail.com>
2021-03-22 19:54:52 -0600

    begin rescue for user update, i think this will kill photo upload completely

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,15 +29,18 @@ class UsersController < ApplicationController
     def update
         @user = User.find(params[:id])
 
-
-        respond_to do |format|
-            if @user.update user_params
-                format.html { redirect_to @user, notice: 'User was successfully updated.' }
-                format.json { render :show, status: :ok, location: @user }
-            else
-                format.html { render :edit }
-                format.json { render json: @user.errors, status: :unprocessable_entity }
+        begin 
+            respond_to do |format|
+                if @user.update user_params
+                    format.html { redirect_to @user, notice: 'User was successfully updated.' }
+                    format.json { render :show, status: :ok, location: @user }
+                else
+                    format.html { render :edit }
+                    format.json { render json: @user.errors, status: :unprocessable_entity }
+                end
             end
+        rescue 
+            #sadness
         end
     end
 
