50027c570561e85878e881bf9d8ea37390feb974
Brent Cameron <bjcameron@gmail.com>
2021-03-22 20:35:52 -0600

    seems like i found a key missing piece here: https://devcenter.heroku.com/articles/direct-to-s3-image-uploads-in-rails, carrying on tomorrow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,4 +56,8 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:name, :email, :profile_photo)
     end
+
+    def set_s3_direct_post 
+        @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+    end
 end
